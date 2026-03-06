caa69471cb88ec51d5361c4bd57f8d8a861f0344
dorname <lgqfighting@163.com>
2026-03-06 17:32:18 +0800

    提交channel实验

--- exercises/01_concurrency_sync/03_channel/src/lib.rs
+++ exercises/01_concurrency_sync/03_channel/src/lib.rs
@@ -14,11 +14,18 @@ use std::thread;
 /// Create a producer thread that sends each element from items into the channel.
 /// The main thread receives all messages and returns them.
 pub fn simple_send_recv(items: Vec<String>) -> Vec<String> {
-    // TODO: Create channel
-    // TODO: Spawn thread to send each element in items
-    // TODO: In main thread, receive all messages and collect into Vec
+    // Create channel
+    let (tx, rx) = mpsc::channel::<String>();
+    // Spawn thread to send each element in items
+    thread::spawn(move || {
+        for item in items {
+            tx.send(item).unwrap();
+        }
+    });
+    // In main thread, receive all messages and collect into Vec
+    let result = rx.into_iter().collect();
+    result
     // Hint: When all Senders are dropped, recv() returns Err
-    todo!()
 }
 
 /// Create `n_producers` producer threads, each sending a message in format `"msg from {id}"`.
@@ -26,11 +33,31 @@ pub fn simple_send_recv(items: Vec<String>) -> Vec<String> {
 ///
 /// Hint: Use `tx.clone()` to create multiple senders. Note that the original tx must also be dropped.
 pub fn multi_producer(n_producers: usize) -> Vec<String> {
-    // TODO: Create channel
-    // TODO: Clone a sender for each producer
-    // TODO: Remember to drop the original sender, otherwise receiver won't finish
-    // TODO: Collect all messages and sort
-    todo!()
+    // Create channel
+    let (tx, rx) = mpsc::channel::<String>();
+    // Clone a sender for each producer
+    let handles = (0..n_producers).map(|i| {
+        let tx = tx.clone();
+        thread::spawn(move || {
+        tx.send(format!("msg from {}",i)).unwrap();
+        })
+    }).collect::<Vec<_>>();
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    // 单线程保证了有序性但不是并发了
+    // thread::spawn(move || {
+    // for i in 0..n_producers {
+    //     let tx = tx.clone();
+    //     tx.send(format!("msg from {}",i)).unwrap();
+    // }});
+    // Remember to drop the original sender, otherwise receiver won't finish
+    drop(tx);
+    // Collect all messages and sort
+    let mut result = rx.into_iter().collect::<Vec<_>>();
+    // 消除并发的无序性
+    result.sort();
+    result
 }
 
 #[cfg(test)]
